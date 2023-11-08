24cde17adc41b9e1d468deae32ebf890c5d4d5c7
Weixi Tong <weixitong@hust.edu.cn>
2023-11-08 11:36:50 +0800

    [feat] Add views and router

---
+++ src/Frontend/src/views/AboutView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="box">
+        <div class="content">
+            <h2 class="topic">软工项目-智能简历解析系统</h2>
+            <br>
+            <br>
+            <div class="desc2">开发人员： 秦声鸿 胡宇飞 童维希 罗琳程</div>
+        </div>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.box {
+    display: flex;
+    justify-content: center;
+
+    .content {
+        margin-top: 20vh;
+        margin-bottom: 2vh;
+
+        .topic {
+            font-size: 4.5rem;
+            text-align: center;
+            background-image: -webkit-linear-gradient(45deg, #43b375 25%, #3e5df7);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            margin-bottom: 0;
+        }
+
+        .desc {
+            text-align: center;
+            font-size: 1.6rem;
+            color: grey;
+        }
+
+        .desc2 {
+            text-align: center;
+            font-size: 2.2rem;
+            color: grey;
+        }
+
+        .waring {
+            margin-top: 5vh;
+            text-align: center;
+            font-size: 2.6rem;
+        }
+    }
+}
+</style>
