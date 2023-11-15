17361f5f7b0670b6931ed89937e4fce0339ed8ed
Col-lin <3078046011@qq.com>
2023-11-15 21:16:48 +0800

    finish Express

--- src/Express/app.js
+++ src/Express/app.js
@@ -50,11 +50,13 @@ async function renderDocx(docxPath, res) {
         .join('\n')
         .split('\n')
         .filter(str => str.trim().length > 0)
+    // 把字符串中所有长度大于1的连续空格缩短为长度为1的空格
+    text = text.map(e => e.replace(/ {2,}/g, ' '))
 
-    const root = parse(html) // 解析HTML为根节点对象
-    const topLevelElements = root.childNodes.filter(node => node.nodeType === 1) // 选择顶层元素节点
+    // const root = parse(html) // 解析HTML为根节点对象
+    // const topLevelElements = root.childNodes.filter(node => node.nodeType === 1) // 选择顶层元素节点
 
-    const text = topLevelElements.map(node => node.text).join('\n'); // 将所有文本内容拼接为一个字符串，并用换行符分隔
+    // const text = topLevelElements.map(node => node.text).join('\n'); // 将所有文本内容拼接为一个字符串，并用换行符分隔
 }
 
 const del_file = FilePath => {
